c0d8cc5a1b463f1c54cc243cc9f3494eb4e3c23f
Jonathan Gao <gsmlg.com@gmail.com>
2026-07-02 11:36:31 +0800

    chore(deps): update Hex packages
    Add RustlerPrecompiled checksum metadata for the published denox_nif v0.6.0 artifacts so updated rustler_precompiled can verify cached archives.

---
+++ checksum-Elixir.Denox.Native.exs
@@ -0,0 +1,6 @@
+%{
+  "libdenox_nif-v0.6.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:e6cdfe219c50b1b9c11fae9dd70776c1d1af61984526747fe414383bf6500107",
+  "libdenox_nif-v0.6.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:7b19edec1b2138047382eb445f380960329c084bc7342ec6c9ad656355612579",
+  "libdenox_nif-v0.6.0-nif-2.17-x86_64-apple-darwin.so.tar.gz" => "sha256:3a3d268bc854e19a988c49022ce1cf8ada8601fd43d274d0360e5f095668de36",
+  "libdenox_nif-v0.6.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:89f19985e1bacf55956bd046e6b50e67dadd6821598b19375c70c7eb59b005f6"
+}
